9c3fbd2b24945289d55caea70e07500daaf6a284
Caio Herrera <caio.herrera@outlook.com>
2016-06-04 19:34:23 -0300

    Adding Procfile

---
+++ static/old/general.js
@@ -0,0 +1,6 @@
+function clearLogin() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    email.value = "";
+    password.value = "";
+}
